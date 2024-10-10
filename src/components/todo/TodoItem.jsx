f1063f154ce8328c846de34675d7b2791fe19681
ohos46419@gmail.com <rjswm5848>
2024-10-10 22:29:11 +0900

    style: 투두 아이템 스타일 추가

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,21 +1,69 @@
+import styled from "styled-components";
+
 const TodoItem = ({ todo, toggleCompleted, handleDelete }) => {
   return (
-    <li key={todo.id}>
-      <p
-        style={{
-          textDecoration: todo.completed ? "line-through" : "none",
-        }}
-      >
-        {todo.text} - {todo.completed ? "완료됨" : "미완료"}
-      </p>
+    <TaskItem key={todo.id}>
+      <TaskItemContent>
+        <p
+          style={{
+            textDecoration: todo.completed ? "line-through" : "none",
+          }}
+        >
+          {todo.text} - {todo.completed ? "완료됨" : "미완료"}
+        </p>
+      </TaskItemContent>
 
-      <button onClick={() => toggleCompleted(todo.id)}>
-        {todo.completed ? "되돌리기" : "완료"}
-      </button>
-
-      <button onClick={() => handleDelete(todo.id)}>삭제</button>
-    </li>
+      <TaskItemActions>
+        <TaskItemActionButton
+          onClick={() => toggleCompleted(todo.id)}
+          color="#582be7"
+        >
+          {todo.completed ? "되돌리기" : "완료"}
+        </TaskItemActionButton>
+        <TaskItemActionButton
+          onClick={() => handleDelete(todo.id)}
+          color="#f05656"
+        >
+          삭제
+        </TaskItemActionButton>
+      </TaskItemActions>
+    </TaskItem>
   );
 };
 
 export default TodoItem;
+
+const TaskItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: #ffffff;
+`;
+
+const TaskItemContent = styled.div``;
+
+const TaskItemActions = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TaskItemActionButton = styled.button`
+  color: #fff;
+  background-color: ${({ color }) => color};
+
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+
+  cursor: pointer;
+
+  &:hover {
+    opacity: 80%;
+  }
+`;
